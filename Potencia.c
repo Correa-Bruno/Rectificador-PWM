41342ff6408e6ac867a64edb73571849700c3ea2
mati_errecart <matierrecart15@gmail.com>
2020-08-10 23:22:37 -0300

    Funciona todo tal cual esta escrito.

--- Potencia.c
+++ Potencia.c
@@ -1,5 +1,5 @@
 //######################################################### SISTEMAS DIGITALES IV ##################################################################################################################
-
+// ALUMNOS: Correa Bruno, De Battista Cristian; Errecart Matias
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #define F_CPU 16000000ul
@@ -12,6 +12,11 @@ int modoant = 0;
 int contador = 0;
 int i,k;
 int indice = 0;
+int cData;
+int DSPI;
+int cen=0;
+int dec=0;
+int uni=0;
 
 long ValorPote;
 
@@ -22,25 +27,28 @@ char cambio[]="E";
 char modo[5];
 char estado[5];
 char valor[5];
-char DATO[5];												// String para armar cadena de caracteres
+char DATO[5];									// String para armar cadena de caracteres
 
 unsigned char Resto;
 
-volatile unsigned char bPB8=1;								// Bandera estado de PB0
-volatile unsigned char bTX=0;								// Bandera para transmitir dato
+volatile unsigned char bPB8=1;					// Bandera estado de PB0
+volatile unsigned char bTX=0;					// Bandera para transmitir dato
 
-void CONTROL(char DATO[]);									// Funcion control PWM
-void onoff(void);
-uint8_t LecturaPin8(void);
+void CONTROL(char DATO[]);						// Funcion control PWM por puerto serie
+void onoff(void);								// Funicion para mostrar estado PWM
+void SPI_MasterTransmit(int cData);				// Funcion para transimitir al display
+void SPI_initial(void);							// Funcion inicializar display
+uint8_t LecturaPin7(void);						// Funcion de lectura de PD7 int On_Off_PWM			
+
 
 int main(void)
 {
 	strcpy(modo,local);
 	
-	DDRD |=(1<<DDD5);										// PD5 como salida PWM (OC0B)
-	DDRB |=(1<<DDB5)|(1<<DDB4)|(0<<DDB0)|(1<<DDB3);			// Conf como salida PB5(13), PB4(12) y PB0(8) como entrada
-	PORTD |= (1<<PORTD2)|(1<<PORTD3);						// Configuro Restistencias pull up
-	PORTB |= (1<<PORTB0);									// Resistencia Pull-UP
+	DDRD |=(1<<DDD5)|(0<<DDD7);			// PD5 como salida PWM (OC0B) y PD7(7) como entrada (On/Off_PWM en modo local)
+	DDRB |=(1<<DDB5)|(1<<DDB3)|(1<<DDB2)|(1<<DDB1)|(1<<DDB0);	// Config como salida PB5(13,CSK), PB3(11,MOSI), PB2(10,SS), PB0(8)semiciclo positivo, PB1(9) semiciclo negativo
+	PORTD |= (1<<PORTD2)|(1<<PORTD3)|(1<<PORTD7);			// Configuro Restistencias pull up INT0, INT1, On_Off_PWM
+	PORTB |= (1<<PORTB2);									// Resistencia Pull-UP
 
 	TCCR0A= (0<<COM0B1)|(0<<COM0B0)|(0<<WGM01)|(1<<WGM00);	// PWM fase correcta,
 	TCCR0B= (0<<CS02)|(1<<CS01)|(0<<CS00)|(1<<WGM02);		// Selecciono prescaler en 8
@@ -48,8 +56,8 @@ int main(void)
 	OCR0A= 199;												// Define la frecuencia pwm 5KHz
 	OCR0B = 100;											// Define el ancho de pulso
 
-	EIMSK= (1<<INT1)|(1<<INT0);
-	EICRA= (1<<ISC11)|(0<<ISC10)|(1<<ISC01)|(1<<ISC00);		// Conf INT1 flanco descendente, INT0 flanco ascendente
+	EIMSK= (1<<INT1)|(1<<INT0);								// Config INT0 INT1
+	EICRA= (1<<ISC11)|(0<<ISC10)|(1<<ISC01)|(1<<ISC00);		// Config INT1 flanco descendente, INT0 flanco ascendente
 
 	TCCR1A=0;
 	TCCR1B=4;												// Selector de reloj CKl/256
@@ -63,7 +71,10 @@ int main(void)
 
 	ADMUX= (1<<REFS0);										// Tension de referencia con capacitor externo
 	ADCSRA=(1<<ADEN)|(0<<ADSC)|(1<<ADPS1)|(1<<ADPS0);		// Habilito el ADC, Configuro prescaler en 8
-
+	
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);					// Configuracion del SPI Modo Maestro, frecuencia SCK 1Mhz
+	SPI_initial();
+	
 	sei();
 	
     while(1)
@@ -72,9 +83,9 @@ int main(void)
 		{
 			bTX=0;											//pongo en 0 la bandera
 		}
-		if ((strcmp(modo,local)==0)&&(LecturaPin8()==1))	//si es modo local leo el y PB0 esta en 1
+		if ((strcmp(modo,local)==0)&&(LecturaPin7()==1))	//si es modo local leo el y PB0 esta en 1
 		{														
-			TCCR0A ^= (1<<COM0B1);		        //Cambio de estado PWM
+			TCCR0A ^= (1<<COM0B1);						   //Cambio de estado PWM
 		    onoff();
 	    }
 		if ((strcmp(modo,remoto)==0)&&(strcmp(estado,on_off)==0))
@@ -83,11 +94,53 @@ int main(void)
 			strcpy(estado,cambio);
 			onoff();
 		}	
-		_delay_ms(250);
+		if(contador==0)
+		{
+			PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+			_delay_us(1);
+			DSPI = 0x09;
+			SPI_MasterTransmit(DSPI);
+			_delay_us(1);
+			DSPI = 0x00;						//Sin decodificación
+			SPI_MasterTransmit(DSPI);
+			PORTB = (1<<PORTB2);				//Indico fin de transferencia
+			_delay_us(1);
+			
+			PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+			_delay_us(1);
+			DSPI = 0x01;						//Bit 0
+			SPI_MasterTransmit(DSPI);
+			_delay_us(1);
+			DSPI = 0x47;
+			SPI_MasterTransmit(DSPI);
+			PORTB = (1<<PORTB2);				//Indico fin de transferencia
+			_delay_us(1);
+			
+			PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+			_delay_us(1);
+			DSPI = 0x02;						//Bit 1
+			SPI_MasterTransmit(DSPI);
+			_delay_us(1);
+			DSPI = 0x47;
+			SPI_MasterTransmit(DSPI);
+			PORTB = (1<<PORTB2);				//Indico fin de transferencia
+			_delay_us(1);
+			
+			PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+			_delay_us(1);
+			DSPI = 0x03;						//Bit 2
+			SPI_MasterTransmit(DSPI);
+			_delay_us(1);
+			DSPI =0x7E;
+			SPI_MasterTransmit(DSPI);
+			PORTB = (1<<PORTB2);				//Indico fin de transferencia
+			_delay_us(1);
+		}
+		_delay_ms(25);
 	}
 }
 
-//######################################################### FUNCION DE CONTROL GENERAL #######################################################################################
+//######################################################### FUNCION PARA ESTABLECER CONTROL LOCAL/REMOTO #######################################################################################
 
 void CONTROL(char DATO[])
 {
@@ -146,67 +199,71 @@ void CONTROL(char DATO[])
 	}
 }
 
-//######################################################### FUNCION PARA LEER EL PIN 8 EN MODO LOCAL ##################################################################################################################
+//######################################################### FUNCION PARA ENCENDER/APAGAR PWM EN MODO LOCAL ##################################################################################################################
 
-uint8_t LecturaPin8(void)
+uint8_t LecturaPin7(void)
 {
-	if((PINB & (1<<PINB0)) == 0)							// Si el botón esta presionado.
+	if((PIND & (1<<PIND7)) == 0)					// Si el botón esta presionado.
 	{ 
-		_delay_ms(25);										// Retardo de entrada para el valor leído.
+		_delay_ms(50);								// Retardo de entrada para el valor leído.
 	}
-	if((PINB & (1<<PINB0)) == 0)							// Verifica que la la lectura sea correcta.
+	if((PIND & (1<<PIND7)) == 0)					// Verifica que la la lectura sea correcta.
 	{ 
-		return 1;											// Si todavía es 0 es porque si teníamos pulsado el botón
+		return 1;									// Si todavía es 0 es porque si teníamos pulsado el botón
 	}
 	else 
 	{	
-		return 0;											// Si el valor cambio la lectura es incorrecta.
+		return 0;									// Si el valor cambio la lectura es incorrecta.
 	}		
 }
 
-//######################################################### FUNCION DE RECEPCION DE DATOS ##################################################################################################################
+//######################################################### VECTOR DE INTERRUPCION RECEPCION DE DATOS ##################################################################################################################
 
-ISR(USART_RX_vect){
-	char datoRX=UDR0;
+ISR(USART_RX_vect)
+{
+	char datoRX=UDR0;	// Leemos el buffer de recepcion
 	
-	if(datoRX=='$')											// si se cumple
+	if(datoRX=='$')		// si se cumple pone en 1 la bandera de dato recibido
 	{
 		bTX=1;
 		indice=0;
 	}
 	else
 	{
-		if((datoRX>='0') &&  (datoRX<='9'))
+		if((datoRX>='0') &&  (datoRX<='9'))		// Si es un numero lo guarda en DATO
 		{
 			DATO[indice]=datoRX;
-			indice++;
-			}else{
-			indice=0;
-			DATO[indice]=datoRX;
-			DATO[1]='\0';
-			DATO[2]='\0';
-		}
+			indice++;							
+			}
+				else
+				{				// Si es una letra pone indice en 0, guarda el dato recibido y limpia lo demas
+				indice=0;
+				DATO[indice]=datoRX;
+				DATO[1]='\0';
+				DATO[2]='\0';
+				}
 	}
 	CONTROL(DATO);
 	return;
 }
+
 
 //######################################################### RUTINA DE TRATAMIENTO DE INTERRUPCION EXTERNA ##################################################################################################################
 
 ISR (INT1_vect) // PD3(pin3)
 {
 
-PORTB &= ! (1<<PORTB4); // En 0 PINB4
-_delay_us(100);
-PORTB |= (1<<PORTB5); // En 1 PB5(13) Semiciclo negativo
+	PORTB &= ! (1<<PORTB0); // En 0 PINB0
+	_delay_us(100);
+	PORTB |= (1<<PORTB1); // En 1 PB1(9) Semiciclo negativo
 
 }
 
 ISR (INT0_vect) // PD2(pin2)
 {
-PORTB &= !(1<<PORTB5); // Apago  PB5
-_delay_us(100);
-PORTB |= (1<<PORTB4); // En 1 PB4(12) Semiciclo positivo
+	PORTB &= !(1<<PORTB1); // Apago  PB1
+	_delay_us(100);
+	PORTB |= (1<<PORTB0); // En 1 PB0(8) Semiciclo positivo
 
 }
 
@@ -214,27 +271,60 @@ PORTB |= (1<<PORTB4); // En 1 PB4(12) Semiciclo positivo
 
 ISR(TIMER1_OVF_vect)
 {
-	ADCSRA|=(1<<ADSC);										// Iniciamos conversion
+	ADCSRA |= (1<<ADSC);										// Iniciamos conversion
 	while(ADCSRA & (1<<ADSC));
 	if (strcmp(modo,local)==0)
 	{
-		ValorPote=ADC;
-		ValorPote= (long)ValorPote*100/1023;				// Guardamos el valor ADC en una variable
+		ValorPote = ADC;
+		ValorPote = (long)ValorPote*100/1023;				// Guardamos el valor ADC en una variable
 	}
-	UDR0=(ValorPote/100)+48;
-	while(!(UCSR0A & (1<<UDRE0)));							// Espera a que se envíe el dato
-	Resto=(ValorPote%100);
-	UDR0=(Resto/10)+48;
+	
+	cen = (ValorPote/100);
+	PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+	_delay_us(1);
+	DSPI = 0x03;						//Bit 2
+	SPI_MasterTransmit(DSPI);
+	_delay_us(1);
+	DSPI = (0x00 + cen);
+	SPI_MasterTransmit(DSPI);
+	PORTB = (1<<PORTB2);				//Indico fin de transferencia
+	_delay_us(1);
+	UDR0 = cen + 48;
+	while(!(UCSR0A & (1<<UDRE0)));		// Espera a que se envíe el dato
+	
+	Resto = (ValorPote%100);
+	
+	dec = (Resto/10);
+	PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+	_delay_us(1);
+	DSPI = 0x02;						//Bit 1
+	SPI_MasterTransmit(DSPI);
+	_delay_us(1);
+	DSPI = (0x00 + dec);
+	SPI_MasterTransmit(DSPI);
+	PORTB = (1<<PORTB2);				//Indico fin de transferencia
+	_delay_us(1);
+	UDR0 = dec + 48;
 	while(!(UCSR0A & (1<<UDRE0)));
-	UDR0=((Resto%10)+48);
+	
+	uni = (Resto%10);
+	PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+	_delay_us(1);
+	DSPI = 0x01;						//Bit 0
+	SPI_MasterTransmit(DSPI);
+	_delay_us(1);
+	DSPI = (0x00 + uni);
+	SPI_MasterTransmit(DSPI);
+	PORTB = (1<<PORTB2);				//Indico fin de transferencia
+	_delay_us(1);
+	UDR0 = (uni + 48);
 	while(!(UCSR0A & (1<<UDRE0)));
-	UDR0=10;
+	UDR0 = 10;
 	while(!(UCSR0A & (1<<UDRE0)));
-	TCNT1= 30360;											// Espera 1 seg para realizar la conversion
-	OCR0B= ValorPote*199/100;								// PD5 salida PWM
+	TCNT1 = 30360;						// Espera 1 seg para realizar la conversion
+	OCR0B = ValorPote*199/100;			// PD5 salida PWM
 }
-
-//######################################################### FUNCION DE MOSTRAR ON - OFF ##################################################################################################################
+//######################################################### FUNCION DE MOSTRAR ON/OFF ##################################################################################################################
 
 void onoff ()
 {
@@ -253,15 +343,124 @@ void onoff ()
 		{
 			while ( !(UCSR0A &(1<<UDRE0)));
 			UDR0=on[i];
+			SPI_initial();
 		}
 	}
 	if ((contador == 0))
 	{
-		for (i=0;i<=strlen(off);i++)
-		{
-			while ( !(UCSR0A &(1<<UDRE0)));
-			UDR0=off[i];
-		}
+		
+			for (i=0;i<=strlen(off);i++)
+			{
+				while ( !(UCSR0A &(1<<UDRE0)));
+				UDR0=off[i];	
+			}
+			
 	}
 	return;
 }
+//######################################################### SPI_MasterTransmit ##################################################################################################################
+
+void SPI_MasterTransmit(int cData)
+{
+	SPDR = cData;
+	while(!(SPSR & (1<<SPIF)))
+	;
+}
+
+//################################################################ SPI ##################################################################################################################
+
+void SPI_initial()
+{
+	////////////////////////////////////////// Intensidad de brillo /////////////////////////////////////////
+	
+	PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+	_delay_us(1);
+	DSPI = 0x0A;						//Intensidad minima
+	SPI_MasterTransmit(DSPI);
+	_delay_us(1);
+	DSPI = 0x00;						//Decodificación de código B para dígitos 7-0
+	SPI_MasterTransmit(DSPI);
+	PORTB = (1<<PORTB2);				//Indico fin de transferencia
+	_delay_us(1);
+	
+	//////////////////////////////////////// Modo de decodificacion /////////////////////////////////////////
+	
+	PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+	_delay_us(1);
+	DSPI = 0x09;						//Decodificación de código B para dígitos 7-0
+	SPI_MasterTransmit(DSPI);
+	_delay_us(1);
+	DSPI = 0xFF;						//Decodificación de código B para dígitos 7-0
+	SPI_MasterTransmit(DSPI);
+	PORTB = (1<<PORTB2);				//Indico fin de transferencia
+	_delay_us(1);
+	
+	////////////////////////////////////////// Cantidad de digitos /////////////////////////////////////////
+	
+	PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+	_delay_us(1);
+	DSPI = 0x0B;						//Mostrar 3 digitos
+	SPI_MasterTransmit(DSPI);
+	_delay_us(1);
+	DSPI = 0x02;						//Decodificación de código B para dígitos 7-0
+	SPI_MasterTransmit(DSPI);
+	PORTB = (1<<PORTB2);				//Indico fin de transferencia
+	_delay_us(1);
+	
+	////////////////////////////////////////// Modo de operacion /////////////////////////////////////////
+	
+	PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+	_delay_us(1);
+	DSPI = 0x0C;						//Opercion normal
+	SPI_MasterTransmit(DSPI);
+	_delay_us(1);
+	DSPI = 0x01;						//Decodificación de código B para dígitos 7-0
+	SPI_MasterTransmit(DSPI);
+	PORTB = (1<<PORTB2);				//Indico fin de transferencia
+	_delay_us(1);
+	
+	////////////////////////////////////////// Modo de testeo /////////////////////////////////////////
+	
+	PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+	_delay_us(1);
+	DSPI = 0x0F;						//Opercion normal
+	SPI_MasterTransmit(DSPI);
+	_delay_us(1);
+	DSPI = 0x00;						//Decodificación de código B para dígitos 7-0
+	SPI_MasterTransmit(DSPI);
+	PORTB = (1<<PORTB2);				//Indico fin de transferencia
+	_delay_us(1);
+	
+	////////////////////////////////////////// Inician todos apagados /////////////////////////////////////////
+	
+	PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+	_delay_us(1);
+	DSPI = 0x01;						//Bit 0
+	SPI_MasterTransmit(DSPI);
+	_delay_us(1);
+	DSPI = 0xFF;
+	SPI_MasterTransmit(DSPI);
+	PORTB = (1<<PORTB2);				//Indico fin de transferencia
+	_delay_us(1);
+	
+	PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+	_delay_us(1);
+	DSPI = 0x02;						//Bit 1
+	SPI_MasterTransmit(DSPI);
+	_delay_us(1);
+	DSPI = 0xFF;
+	SPI_MasterTransmit(DSPI);
+	PORTB = (1<<PORTB2);				//Indico fin de transferencia
+	_delay_us(1);
+	
+	PORTB = (0<<PORTB2);				//Indico inicio de transferencia
+	_delay_us(1);
+	DSPI = 0x03;						//Bit 2
+	SPI_MasterTransmit(DSPI);
+	_delay_us(1);
+	DSPI = 0xFF;
+	SPI_MasterTransmit(DSPI);
+	PORTB = (1<<PORTB2);				//Indico fin de transferencia
+	_delay_us(1);
+}
+
